692f67595449e80e6aa867c2a9cc93bea945b590
Ar Rakin <rakinar2@onesoftnet.eu.org>
2023-06-12 19:51:23 +0600

    fix: sidebar sizing issues

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,8 @@
-import { MenuList } from "@mui/material";
+import { MenuList, Tooltip } from "@mui/material";
 import { useState } from "react";
-import { MdMenu } from 'react-icons/md';
+import { MdMenu } from "react-icons/md";
 import { useLocation } from "react-router-dom";
-import { routes } from '../router';
+import { routes } from "../router";
 import SidebarItem from "./SidebarItem";
 
 export default function Sidebar() {
@@ -10,10 +10,33 @@ export default function Sidebar() {
     const [expanded, setExpanded] = useState(false);
 
     return (
-        <div className={"px-3 mr-3 flex bg-[#1f1e1e] " + (!expanded ? "w-[50px]" : "w-[auto] max-w-[10vw]")}>
+        <div className={"relative px-3 mr-3 bg-[#1f1e1e] " + (!expanded ? "w-[50px]" : "w-[20em]")}>
             <MenuList>
-                <SidebarItem onClick={() => setExpanded(s => !s)} Icon={MdMenu} expanded={false} topIcon={true} style={{ marginTop: 0 }} />
-                {routes.map(({ icon, path, title }, index) => <SidebarItem expanded={expanded} key={index} title={title} Icon={icon} active={path === currentPath} />)}
+                <div className="mb-[10px]">
+                    <Tooltip
+                        title="Toggle Menu"
+                        enterDelay={1200}
+                    >
+                        <div className="inline-block">
+                            <SidebarItem
+                                onClick={() => setExpanded((s) => !s)}
+                                Icon={MdMenu}
+                                expanded={false}
+                                topIcon={true}
+                                style={{ marginTop: 0, marginBottom: 0 }}
+                            />
+                        </div>
+                    </Tooltip>
+                </div>
+                {routes.map(({ icon, path, title }, index) => (
+                    <SidebarItem
+                        expanded={expanded}
+                        key={index}
+                        title={title}
+                        Icon={icon}
+                        active={path === currentPath}
+                    />
+                ))}
             </MenuList>
         </div>
     );
